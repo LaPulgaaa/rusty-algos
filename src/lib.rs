ad8867a86862ad104a742b811d311e679b4e80c3
Varun Singh <varunsingh9114@gmail.com>
2024-07-06 13:47:31 +0530

    sort: Add optimised bubble sort algo.
    Time Complexity: O(N^2)
    Space Complexity: O(1)
    
    Best Case: O(N)

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,9 @@ mod tests {
         let arr = vec![4,2,8,5,1];
         let res = crate::sort::bubble_sort(arr);
         assert_eq!(res,vec![1,2,4,5,8]);
+
+        let opt_arr=vec![8,4,3,1,14,7];
+        let opt_res = crate::sort::bubble_sort_optimal(opt_arr);
+        assert_eq!(opt_res,vec![1,3,4,7,8,14]);
     }
 }
--- src/sort.rs
+++ src/sort.rs
@@ -10,3 +10,24 @@ pub fn bubble_sort(mut arr: Vec<i32>)->Vec<i32>{
     }
     arr
 }
+
+pub fn bubble_sort_optimal(mut arr: Vec<i32>)->Vec<i32>{
+    let mut swapped = false;
+    
+    for i in 0..arr.len()-1{
+        for j in 0..arr.len()-i-1{
+            if arr[j]>arr[j+1]{
+                swapped=true;
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        if swapped==false{
+            return arr;
+        }
+    }
+
+    arr
+
+}
