3996dad3d9ff2b9330c7e43ebc57eb35586d90bd
Varun Singh <varunsingh9114@gmail.com>
2024-07-07 13:22:36 +0530

    sort: Add selection sort.
    Time complexity: O(N^2)
    Space complexity: O(1) for optimised approach. O(length) when
    using a temporary array.

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,17 @@ mod tests {
     #[test]
     fn test_sort(){
         let arr = vec![4,2,8,5,1];
-        let res = crate::sort::bubble_sort(arr);
+        let res = crate::sort::bubble_sort(arr.clone());
         assert_eq!(res,vec![1,2,4,5,8]);
 
         let opt_arr=vec![8,4,3,1,14,7];
         let opt_res = crate::sort::bubble_sort_optimal(opt_arr);
         assert_eq!(opt_res,vec![1,3,4,7,8,14]);
+
+        let sel_arr = crate::sort::select_sort(arr.clone());
+        assert_eq!(sel_arr, vec![1,2,4,5,8]);
+
+        let sel_opt_arr = crate::sort::select_sort_opt(arr.clone());
+        assert_eq!(sel_opt_arr, vec![1,2,4,5,8]);
     }
 }
--- src/sort.rs
+++ src/sort.rs
@@ -1,3 +1,4 @@
+// O(N^2) O(1) stable
 pub fn bubble_sort(mut arr: Vec<i32>)->Vec<i32>{
     for i in 0..arr.len()-1{
         for j in 0..arr.len()-i-1{
@@ -11,6 +12,7 @@ pub fn bubble_sort(mut arr: Vec<i32>)->Vec<i32>{
     arr
 }
 
+// O(N^2) O(1) stable
 pub fn bubble_sort_optimal(mut arr: Vec<i32>)->Vec<i32>{
     let mut swapped = false;
     
@@ -30,4 +32,57 @@ pub fn bubble_sort_optimal(mut arr: Vec<i32>)->Vec<i32>{
 
     arr
 
+}
+
+// O(N^2) O(len) unstable
+pub fn select_sort(mut arr: Vec<i32>)->Vec<i32>{
+    
+    let len :usize= arr.len();
+
+    if len==1  || len==0 {
+        return arr;
+    }
+
+    let mut temp = Vec::new();
+
+    let mut i=0;
+
+    while i < len{
+        let mut min_index = 0;
+        for j in 1..arr.len(){
+            if arr[min_index]>arr[j] {
+                min_index = j;
+            }
+        }
+
+        temp.push(arr[min_index]);
+        arr[min_index]=i32::MAX;
+        i += 1;
+    }
+
+    return temp;
+}
+
+// O(N^2) O(1) unstable
+pub fn select_sort_opt(mut arr: Vec<i32>)->Vec<i32>{
+
+    if arr.len()==0 || arr.len()==1 {
+        return arr;
+    }
+
+    for i in 0..arr.len(){ 
+        let mut rest_min=i;
+
+        for j in i+1..arr.len(){
+            if arr[j]<arr[rest_min]{
+                rest_min = j;
+            }
+        }
+
+        let temp = arr[rest_min];
+        arr[rest_min] = arr[i];
+        arr[i] = temp;
+    }
+
+    return arr;
 }
