c6e5a646bb769051a05d2933e1f6ed34e0f7f8a9
Varun Singh <varunsingh9114@gmail.com>
2024-11-17 18:05:15 +0530

    misc: Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.

--- src/misc.rs
+++ src/misc.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp::{min, Ordering},
-    collections::HashMap,
+    collections::{HashMap, HashSet},
 };
 
 // minimum difference in an array
@@ -572,4 +572,28 @@ pub fn merge(arg: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     }
 
     res
+}
+
+pub fn set_zeroes(matrix: &mut [Vec<i32>]) {
+    let mut marked_col: HashSet<usize> = HashSet::new();
+
+    for row in matrix.iter_mut() {
+        let has_zero = row.iter().any(|x| *x == 0);
+
+        if !has_zero {
+            continue;
+        }
+
+        row.iter_mut().enumerate().for_each(|(j, val)| {
+            if *val == 0 {
+                marked_col.insert(j);
+            }
+
+            *val = 0;
+        });
+    }
+
+    for col in marked_col {
+        matrix.iter_mut().for_each(|row| row[col] = 0);
+    }
 }
